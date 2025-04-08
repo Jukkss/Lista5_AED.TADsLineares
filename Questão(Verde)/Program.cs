949fc64dbf07d8be27a97842a6c606fcb1a56cff
João Victor Soares Souza <sjuao7729@gmail.com>
2025-04-08 19:51:55 -0300

    Questão 1, 2, 3, 4, 5 (ComVerde) e 1, 2 (Sem Verde)

---
+++ Questão(Verde)/Program.cs
@@ -0,0 +1,152 @@
+﻿using System;
+
+class Arquivo
+{
+    private string nome;
+    private int paginas;
+
+    public Arquivo(string nome, int paginas)
+    {
+        this.nome = nome;
+        this.paginas = paginas;
+    }
+
+    public string ObterNome()
+    {
+        return nome;
+    }
+
+    public int ObterPaginas()
+    {
+        return paginas;
+    }
+
+    public string Mostrar()
+    {
+        return nome + " " + paginas;
+    }
+}
+
+class Fila
+{
+    private Arquivo[] elementos;
+    private int inicio;
+    private int fim;
+    private int tamanho;
+
+    public Fila(int capacidade)
+    {
+        elementos = new Arquivo[capacidade];
+        inicio = 0;
+        fim = 0;
+        tamanho = 0;
+    }
+
+    public bool EstaVazia()
+    {
+        return tamanho == 0;
+    }
+
+    public bool EstaCheia()
+    {
+        return tamanho == elementos.Length;
+    }
+
+    public bool Inserir(Arquivo arquivo)
+    {
+        if (!EstaCheia())
+        {
+            elementos[fim] = arquivo;
+            fim = (fim + 1) % elementos.Length;
+            tamanho++;
+            return true;
+        }
+        else
+        {
+            Console.WriteLine("Fila cheia!");
+            return false;
+        }
+    }
+
+    public Arquivo Remover()
+    {
+        if (!EstaVazia())
+        {
+            Arquivo removido = elementos[inicio];
+            inicio = (inicio + 1) % elementos.Length;
+            tamanho--;
+            return removido;
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    public void Listar()
+    {
+        if (EstaVazia())
+        {
+            Console.WriteLine("Fila vazia");
+            return;
+        }
+
+        int i = inicio;
+        int cont = 0;
+
+        while (cont < tamanho)
+        {
+            Console.WriteLine(elementos[i].Mostrar());
+            i = (i + 1) % elementos.Length;
+            cont++;
+        }
+    }
+}
+
+class Teste
+{
+    static void Main()
+    {
+        Fila fila = new Fila(100);
+        int opcao = 0;
+
+        while (opcao != 4)
+        {
+            Console.WriteLine("Op:");
+            string entrada = Console.ReadLine();
+            opcao = int.Parse(entrada);
+
+            if (opcao == 1)
+            {
+                string nome = Console.ReadLine();
+                int paginas = int.Parse(Console.ReadLine());
+                Arquivo novo = new Arquivo(nome, paginas);
+                fila.Inserir(novo);
+            }
+            else if (opcao == 2)
+            {
+                Arquivo arquivo = fila.Remover();
+                if (arquivo != null)
+                {
+                    Console.WriteLine(arquivo.ObterNome());
+                }
+                else
+                {
+                    Console.WriteLine("Fila vazia");
+                }
+            }
+            else if (opcao == 3)
+            {
+                fila.Listar();
+            }
+            else if (opcao == 4)
+            {
+                Console.WriteLine("Encerrando...");
+            }
+            else
+            {
+                Console.WriteLine("Opção inválida");
+            }
+        }
+    }
+}
---
+++ Questão4(Verde)/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+
+class Fila
+{
+    private string[] elementos;
+    private int inicio;
+    private int fim;
+    private int tamanho;
+
+    public Fila(int capacidade)
+    {
+        elementos = new string[capacidade];
+        inicio = 0;
+        fim = 0;
+        tamanho = 0;
+    }
+
+    public bool EstaVazia()
+    {
+        return tamanho == 0;
+    }
+
+    public bool EstaCheia()
+    {
+        return tamanho == elementos.Length;
+    }
+
+    public void Inserir(string valor)
+    {
+        if (!EstaCheia())
+        {
+            elementos[fim] = valor;
+            fim = (fim + 1) % elementos.Length;
+            tamanho++;
+        }
+    }
+
+    public string Remover()
+    {
+        if (!EstaVazia())
+        {
+            string valor = elementos[inicio];
+            inicio = (inicio + 1) % elementos.Length;
+            tamanho--;
+            return valor;
+        }
+        return null;
+    }
+
+    public int ObterTamanho()
+    {
+        return tamanho;
+    }
+
+    public void Listar()
+    {
+        for (int i = 0; i < tamanho; i++)
+        {
+            int index = (inicio + i) % elementos.Length;
+            Console.WriteLine(elementos[index]);
+        }
+    }
+
+    public string ObterPrimeiro()
+    {
+        if (!EstaVazia())
+        {
+            return elementos[inicio];
+        }
+        return null;
+    }
+}
+
+class Programa
+{
+    static void Main()
+    {
+        Fila fila = new Fila(5);
+        int opcao = 0;
+
+        while (opcao != 6)
+        {
+            Console.WriteLine("Op:");
+            opcao = int.Parse(Console.ReadLine());
+
+            if (opcao == 1)
+            {
+                Console.WriteLine(fila.ObterTamanho());
+            }
+            else if (opcao == 2)
+            {
+                string removido = fila.Remover();
+                if (removido != null)
+                {
+                    Console.WriteLine(removido);
+                }
+            }
+            else if (opcao == 3)
+            {
+                string id = Console.ReadLine();
+                fila.Inserir(id);
+            }
+            else if (opcao == 4)
+            {
+                fila.Listar();
+            }
+            else if (opcao == 5)
+            {
+                string primeiro = fila.ObterPrimeiro();
+                if (primeiro != null)
+                {
+                    Console.WriteLine(primeiro);
+                }
+            }
+        }
+    }
+}
